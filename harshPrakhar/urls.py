fbcb5720614fe8c23ff03f535b700e720b802eed
12singhprakhar <12singhprakhar@gmail.com>
2025-06-27 14:31:55 +0530

    . loginadded

--- harshPrakhar/urls.py
+++ harshPrakhar/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from .views import index
+from .views import login
+
 urlpatterns = [
     path("",index),
+    path("login/",login),
     path("admin/",admin.site.urls)
 ]
--- harshPrakhar/views.py
+++ harshPrakhar/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 def index(req):
     return render(req,"home.html")
+def login(req):
+    return render(req,"login.html")
